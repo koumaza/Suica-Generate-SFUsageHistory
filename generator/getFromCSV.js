b4fee2290e45dddb2b7575284c21606990db0e81
shanghai yakisoba chan! <koumaza--@outlook.com>
2021-07-30 15:36:45 +0900

    Create getFromCSV.js

---
+++ generator/getFromCSV.js
@@ -0,0 +1,36 @@
+export const getFromCSV = (csv) => {
+  const dataArray = []
+  csv.split('\n').forEach(content => {
+      const data = content.split(',')
+      if (!data[3] == '') {
+          dataArray.push({
+              Date: data[0]+'/'+data[1],
+              Type: [
+                  data[2],
+                  data[3]    
+              ],
+              Location: [
+                  data[4],
+                  data[5]
+              ],
+              Balance: data[6],
+              Amount: data[7]
+          })
+      }
+      else if (data[3] === '') {
+          dataArray.push({
+              Date: data[0]+'/'+data[1],
+              Type: [
+                  data[2] 
+              ],
+              Location: [
+                  data[4],
+                  data[5]
+              ],
+              Balance: data[6],
+              Amount: data[7]
+          })
+      }
+  })
+  return dataArray
+}
